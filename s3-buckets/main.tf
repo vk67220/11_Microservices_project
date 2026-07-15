15fe2524b7ac282175d82b4d7578a35255abeb5e
Varaprasad Kasturi <vk67220@corp.cyient.com>
2026-07-15 18:38:40 +0530

    added

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "bucket1" {
   bucket = var.bucket1_name
-
+  force_destroy = true
   tags = {
     Name        = var.bucket1_name
     Environment = var.environment
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "bucket1" {
 
 resource "aws_s3_bucket_versioning" "bucket1_versioning" {
   bucket = aws_s3_bucket.bucket1.id
-
+  
   versioning_configuration {
     status = "Enabled"
   }
@@ -17,6 +17,7 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 
 resource "aws_s3_bucket" "bucket2" {
   bucket = var.bucket2_name
+  force_destroy = true
 
   tags = {
     Name        = var.bucket2_name
