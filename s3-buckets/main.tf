143af9000158bb01a9b5e71aaa68f3f8cc5310b4
root <root@ip-172-31-92-43.ec2.internal>
2026-07-15 13:13:08 +0000

    added

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket1" {
-  bucket = var.bucket1_name
+  bucket        = var.bucket1_name
   force_destroy = true
   tags = {
     Name        = var.bucket1_name
@@ -9,14 +9,14 @@ resource "aws_s3_bucket" "bucket1" {
 
 resource "aws_s3_bucket_versioning" "bucket1_versioning" {
   bucket = aws_s3_bucket.bucket1.id
-  
+
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = var.bucket2_name
+  bucket        = var.bucket2_name
   force_destroy = true
 
   tags = {
