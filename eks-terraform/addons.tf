39e3cd9e60369b2360ebd51312e52cbb0e84fd25
Varaprasad Kasturi <vk67220@corp.cyient.com>
2026-07-17 23:07:25 +0530

    added

--- eks-terraform/addons.tf
+++ eks-terraform/addons.tf
@@ -1,23 +1,22 @@
 locals {
   cluster_addons = {
     coredns = {
-      #most_recent = true
-      addon_version = "v1.12.4-eksbuild.1"
+      most_recent = true
+     
     }
 
     kube-proxy = {
-      #most_recent = true
-      addon_version = "v1.33.0-eksbuild.1"
+      most_recent = true
+   
     }
 
     vpc-cni = {
-      #most_recent = true
-      addon_version = "v1.20.4-eksbuild.1"
+      most_recent = true
+      
     }
 
     aws-ebs-csi-driver = {
-      #most_recent              = true
-      addon_version            = "v1.49.0-eksbuild.1"
+      most_recent              = true
       service_account_role_arn = aws_iam_role.ebs_csi.arn
     }
   }
